bdb1667fd7e82d3d2afaac1c9161789cc66a0b5a
unknown <Student@IuEKT-13-318-25.hrz.isc.fh-kiel.de>
2019-05-31 10:01:21 +0200

    First commit

---
+++ mk120-lab2/Oscilloscope.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mk120_lab2
+{
+    class Oscilloscope
+    {
+    }
+}
